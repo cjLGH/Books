96a9490b88458415aba942d42834a2b1455df908
cjlGH <cjl@cjlon.com>
2020-01-08 21:59:24 -0600

    second commit

---
+++ part1/describeblocks.test.ps1
@@ -0,0 +1,10 @@
+describe 'Stop-MailDaemon' {
+    ## Stuff goes in here
+}
+
+describe -Tag 'Linux' 'Stop-MailDaemon' {
+    ## Describe-specific code in here
+    context 'When the server is down' {
+        ## Context-specific code in here
+    }
+}
